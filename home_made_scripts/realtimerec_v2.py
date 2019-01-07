314306c0347559391c582f785a20f094c683e8a4
Ethan Mengoreo <mengoreo@163.com>
2019-01-07 17:11:10 +0800

    bug fix

--- home_made_scripts/realtimerec_v2.py
+++ home_made_scripts/realtimerec_v2.py
@@ -7,7 +7,6 @@ Matplotlib and NumPy have to be installed.
 import argparse
 import queue
 import sys
-import subprocess
 
 
 def int_or_str(text):
@@ -46,7 +45,6 @@ if any(c < 1 for c in args.channels):
     parser.error('argument CHANNEL: must be >= 1')
 mapping = [c - 1 for c in args.channels]  # Channel numbers start with 1
 q = queue.Queue()
-count = 0
 
 
 def audio_callback(indata, frames, time, status):
@@ -64,8 +62,7 @@ def update_plot(frame):
     therefore the queue tends to contain multiple blocks of audio data.
 
     """
-    global plotdata, count, freq, fft
-    count += 1
+    global plotdata, maxfft
     tempdata = np.ndarray(shape=(1, plotdata.shape[1]))
     # print(tempdata)
     while True:
@@ -83,11 +80,25 @@ def update_plot(frame):
 
     # IMPORTANT!!!!
     # tempdata = tempdata.reshape(len(tempdata),)
+    # print(tempdata)
+    # np.savetxt('temp.txt', tempdata)
     for i in range(len(args.channels)):
-        tempfft = np.fft.fft(tempdata[:, i].reshape(len(tempdata[:, i]),))
+        coldata = tempdata[:, i]
+        coldata[coldata == np.inf] = 0.0
+        coldata[coldata > 1] = 0.0
+        coldata = coldata.reshape(len(coldata), )
+        tempfft = np.fft.fft(coldata)
         tempfft = np.abs(tempfft)
         tempfreq = np.fft.fftfreq(len(tempfft), 1.0/44100)
         tempfft = tempfft[:int(len(tempfft)/2)]
+        # if maxfft[i] > 0:
+        #     tempfft = tempfft / maxfft[i]
+        # try:
+        #     if np.max(tempfft) > maxfft[i]:
+        #         maxfft[i] = np.max(tempfft)
+
+        # except ValueError:
+        #     pass
         tempfreq = tempfreq[:int(len(tempfreq)/2)]
         if i == 0:
             fft = tempfft
@@ -95,23 +106,50 @@ def update_plot(frame):
             freq = tempfreq
             freq = freq.reshape(len(freq), 1)
         else:
-            l = len(tempfft) - fft.shape[0]
-            if l > 0:
-                fft = np.append(np.zeros(l-1, fft.shape[1]), fft, axis=0)
-                freq = np.append(np.zeros(l-1, fft.shape[1]), freq, axis=0)
-            elif l < 0:
-                tempfft = np.append(np.zeros(-l-1,), tempfft, axis=0)
-                tempfreq = np.append(np.zeros(-l-1,), tempfreq, axis=0)
+            gap = len(tempfft) - fft.shape[0]
+            if gap > 0:
+                fft = np.append(np.zeros(gap-1, fft.shape[1]), fft, axis=0)
+                freq = np.append(np.zeros(gap-1, fft.shape[1]), freq, axis=0)
+            elif gap < 0:
+                tempfft = np.append(np.zeros(-gap-1,), tempfft, axis=0)
+                tempfreq = np.append(np.zeros(-gap-1,), tempfreq, axis=0)
 
             tempfft = tempfft.reshape(len(tempfft), 1)
             tempfreq = tempfreq.reshape(len(tempfreq), 1)
             # print(fft.shape, tempfft.shape)
             fft = np.append(fft, tempfft, axis=1)
             freq = np.append(freq, tempfreq, axis=1)
+
+    # if len(fft) > 0 and np.max(fft) > 1:
+    #     print()
+    #     print(fft)
+    #     print()
+    #     print(tempdata)
+    #     # sys.exit(1)
+    # print(len(args.channels))
+    # for i in range(len(args.channels)):
+    #     coldata = tempdata[:, i]
+    #     coldata = coldata.reshape(len(coldata), )
+    #     coldata[coldata == np.inf] = 0.0
+    #     # print(coldata.shape)
+    #     fft = np.fft.fft(coldata)
+    #     fft = np.abs(fft)
+    #     freq = np.fft.fftfreq(len(fft), 1.0/44100)
+    #     fft = fft[:int(len(fft)/2)]
+    #     freq = freq[:int(len(freq)/2)]
+    #     print()
+    #     print(fft)
+    #     print()
+    #     flines[i].set_data(freq, fft)
+
     for column, line in enumerate(lines):
         line.set_ydata(plotdata[:, column])
     # for line in enumerate(flines):
-    flines.set_data(freq, fft)
+    np.savetxt('fft.txt', fft)
+    np.savetxt('freq.txt', freq)
+    # flines.set_data(freq, fft)
+    for i in range(len(flines)):
+        flines[i].set_data(freq[:, i], fft[:, i])
     return lines, flines,
 
 
@@ -131,6 +169,7 @@ try:
     length = int(args.window * args.samplerate / (1000 * args.downsample))
     plotdata = np.zeros((length, len(args.channels)))
 
+    maxfft = np.zeros((len(args.channels), ))
     fft = np.zeros((length, len(args.channels)))
     freq = np.zeros((length, len(args.channels)))
     # fig, (ax, fx) = plt.subplots(1, 2, sharey=True)
@@ -141,23 +180,28 @@ try:
     fx = fig.add_subplot(122)
 
     lines = ax.plot(plotdata)
-    flines = fx.plot(freq, fft)[0]
+    flines = fx.plot(freq, fft)
+    # flines = []
+    # for i in range(len(args.channels)):
+    #     flines.append(fx.plot(freq, fft)[0])
 
     # Colors
     if len(args.channels) > 1:
         ax.legend(['channel {}'.format(c) for c in args.channels],
-                  loc='lower left', ncol=len(args.channels))
+                  loc='lower left', ncol=len(args.channels),
+                  bbox_to_anchor=(0., 1.02, 1., .102))
         fx.legend(['channel {}'.format(c) for c in args.channels],
-                  loc='lower left', ncol=len(args.channels))
+                  loc='lower left', ncol=len(args.channels),
+                  bbox_to_anchor=(0., 1.02, 1., .102))
 
     ax.axis((0, len(plotdata), -1, 1))
-    fx.axis((0, 44100, 0, 100))
+    fx.axis((0, 44100, 0, 1))
     # ax.set_yticks([0])
     ax.yaxis.grid(True)
     fx.yaxis.grid(True)
     # ax.tick_params(bottom='off', top='off', labelbottom='off',
-                   # right='off', left='off', labelleft='off')
-    # fig.tight_layout(pad=0)
+    #                right='off', left='off', labelleft='off')
+    fig.tight_layout(pad=0)
 
     stream = sd.InputStream(
         device=args.device, channels=max(args.channels),
