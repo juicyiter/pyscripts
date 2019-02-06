507040cfc7fefdecac724560a13d53be0e21326f
Mengoreo <mengoreo@163.com>
2019-02-06 09:34:22 +0800

    updates

--- home_made_scripts/chirp.py
+++ home_made_scripts/chirp.py
@@ -1,8 +1,43 @@
 import sounddevice as sd
 import numpy as np
 import pickle
-with open('chirp.pickle', 'rb') as f:
-	y = pickle.load(f)
+from scipy.signal.waveforms import sweep_poly
+
+
+def generate_chirp():
+	p = np.poly1d([1.4, 16])
+	t = np.linspace(0, 5, 5*48)
+	w = sweep_poly(t, p)
+	w = np.hanning(len(w))*w
+
+	p = np.poly1d([-1.4, 30])
+	t0 = np.linspace(5, 10, 5*48)
+	t = np.append(t, t0)
+	w0 = sweep_poly(t0, p)
+	w0 = np.hanning(len(w0))*w0
+	w = np.append(w, w0)
+
+	p = np.poly1d([1.4, 2])
+	t0 = np.linspace(10, 15, 5*48)
+	t = np.append(t, t0)
+	w0 = sweep_poly(t0, p)
+	w0 = np.hanning(len(w0))*w0
+	w = np.append(w, w0)
+
+	p = np.poly1d([-1.4, 44])
+	t0 = np.linspace(15, 20, 5*48)
+	t = np.append(t, t0)
+	w0 = sweep_poly(t0, p)
+	w0 = np.hanning(len(w0))*w0
+	w = np.append(w, w0)
+	w = w*0.8
+
+	return w
+
+y = generate_chirp()
+y = np.array(y)
+# with open('chirp.pickle', 'rb') as f:
+# 	y = pickle.load(f)
 input_device = 0
 i = 0
 device_list = list(sd.query_devices())
@@ -11,11 +46,13 @@ for device in device_list:
 		input_device = device_list.index(device)
 		break
 
+
+
 def callback(indata, outdata, frames, time, status):
         global i
-        outdata[:, 1] = y
+        outdata = y
 
-with sd.Stream(device=(0, 1),
+with sd.RawStream(device=(0, 1),
                callback=callback,
                blocksize=960,
                samplerate=48000):
