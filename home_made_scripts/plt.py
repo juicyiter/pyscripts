8b245d05104cc2e38b2f38883894cfe7a6c728f6
Mengoreo <mengoreo@163.com>
2019-02-21 19:57:05 +0800

    updates

--- home_made_scripts/plt.py
+++ home_made_scripts/plt.py
@@ -1,7 +1,19 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import pickle
+from scipy import signal
 
 data = np.loadtxt('plotdata.txt')
+# left = pickle.load(open('left_y.pickle', 'rb'))
+# right = pickle.load(open('right_y.pickle', 'rb'))
 
-plt.plot(data[::3])
+fig = plt.figure()
+ax = fig.add_subplot(211)
+bx = fig.add_subplot(212)
+
+ax.plot(data)
+pdata = np.absolute(signal.hilbert(data[:, 1]))
+bx.plot(pdata)
+peaks, _ = signal.find_peaks(pdata, distance=100)
+bx.plot(peaks, pdata[peaks], 'x')
 plt.show()
