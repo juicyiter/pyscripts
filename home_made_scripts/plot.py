17a205b838f114d8d61b535fb920343ba4b78e0f
Mengoreo <mengoreo@163.com>
2019-03-20 20:59:32 +0800

    update plot

--- home_made_scripts/plot.py
+++ home_made_scripts/plot.py
@@ -1,36 +1,24 @@
+import pickle
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.signal import butter, lfilter
 
-ch_num = 3
-raw_data = np.genfromtxt('output.txt')
-print(len(raw_data))
-raw_data = np.append(raw_data, np.zeros(ch_num - len(raw_data) % ch_num))
-print(len(raw_data))
-data = raw_data.reshape(-1, ch_num)
-# t = np.linspace(1, 20, 20*48)
 
-# def butter_bandpass(lowcut, highcut, fs, order=5):
-#     nyq = 0.5*fs
-#     low = lowcut / nyq
-#     high = highcut / nyq
-#     b, a = butter(order, [low, high], btype='band')
-#     return b, a
+data = pickle.load(open('out.pickle', 'rb'))
 
-# def butter_bandpass_filter(data, lowcut, highcut, fs, order=5):
-#     b, a = butter_bandpass(lowcut, highcut, fs, order=order)
-#     y = lfilter(b, a, data)
-#     return y
-# for i in range(len(data)):
-#     for j in range(len(data[i])):
-#         if np.absolute(data[i][j]) > 1:
-#             data[i][j] = 0
-# ##print(data)
-# y = butter_bandpass_filter(data, 16, 23, 48)
-fig = plt.figure(1)
-ax = fig.add_subplot(111)
-lines = ax.plot(data)
-ax.legend(['channel {}'.format(c) for c in range(ch_num)],
-          loc='lower left', ncol=4,
-          bbox_to_anchor=(0., 1.02, 1., .102))
+first = np.zeros(1)
+second = np.zeros(1)
+i = 0
+while i <= (len(data) // 960):
+    first = np.append(first, data[i * 960: (i + 1) * 960])
+    first = np.append(first, np.ones(1) * ((i + 1) % 2))
+    second = np.append(second, data[(i + 1) * 960: (i + 2) * 960])
+    second = np.append(second, np.zeros(1))
+    i += 2
+
+if len(first) > len(second):
+    first = first[:len(second)]
+
+first = np.reshape(first, (len(first), 1))
+second = np.reshape(second, (len(second), 1))
+plt.plot(np.append(first, second, axis=1))
 plt.show()
