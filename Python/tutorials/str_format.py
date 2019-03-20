414546b136d87a0c457c05c07fc19cf5543ea9a1
Mengoreo <mengoreo@163.com>
2019-03-20 19:49:24 +0800

    upload str_format.py

--- Python/tutorials/str_format.py
+++ Python/tutorials/str_format.py
@@ -2,22 +2,23 @@ age = 20
 name = "Ethan"
 
 
-print("{0} was {1} years old".format(name,age))
+print("{0} was {1} years old".format(name, age))
 print("Why {0} is playing python?".format(name))
 
 # Numbers can be omited
-print("{} was {} years old".format(name,age))
+print("{} was {} years old".format(name, age))
 print("Why is {} typing this?".format(name))
 
 # Decimal with precison of 4 for float number.
-print("{0:.4}".format(1.0/3))
+print("{0:0.5}".format(1.0 / 3))
 
 # Fill undercore(_) with the text centered
 # (^) to 20 width
 print("{0:_^20}".format("hello world"))
 
 # Keyword base
-print("{name} is typing with {keyboard}.".format(name = "Ethan",keyboard = "poker"))
+print("{name} is typing with {keyboard}.".format(
+    name="Ethan", keyboard="poker"))
 
 # End with a blank
 # print('a', end = '') to be solved
@@ -31,4 +32,3 @@ print('What\'s your name?')
 
 # Raw text
 print(r"This is a raw text. \n")
-
